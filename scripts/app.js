6c547f364db47118c33cc384d54d00c6514bd779
unfriend <t-moore@mail.ru>
2018-02-14 13:40:42 +0300

    + edit album

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,7 @@
         lastID = parseInt(localStorage.getItem(albumsStorageLastID)) || 0;
     }
     let newAlbumModal, newAlbumNameInput;
+    let editAlbumModals;
 
     console.log("lastID: " + lastID + "; albums: ", albums);
 
@@ -23,12 +24,14 @@
                         <div className="d-flex justify-content-between align-items-center">
                             <div className="btn-group">
                                 <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                <button type="button" className="btn btn-sm btn-outline-danger" onClick={(e) => props.onRemove(album.id)}>Delete</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary" onClick={ () => props.onEditStart(album.id) }>Edit</button>
+                                <button type="button" className="btn btn-sm btn-outline-danger" onClick={ () => props.onRemove(album.id) }>Delete</button>
                             </div>
                             <small className="text-muted">9 photos</small>
                         </div>
                     </div>
+
+                    <ModalEditAlbum value={ album } onEditSave={ (id) => props.onEditSave(album.id) } />
                 </div>
             </div>
         );
@@ -36,7 +39,7 @@
 
     function ModalNewAlbum(props) {
         return (
-            <div className="modal fade" id="new-album-modal-2" tabIndex="-1" role="dialog" aria-hidden="true">
+            <div className="modal fade" id="modal-new-album" tabIndex="-1" role="dialog" aria-hidden="true">
                 <div className="modal-dialog modal-dialog-centered" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
@@ -47,13 +50,42 @@
                         </div>
                         <div className="modal-body">
                             <div className="form-group">
-                                <label htmlFor="new-album-name-2">Название альбома</label>
-                                <input type="text" className="form-control" id="new-album-name-2" />
+                                <label htmlFor="input-new-album-name">Название альбома</label>
+                                <input type="text" className="form-control" id="input-new-album-name" />
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
                             <button type="button" className="btn btn-primary" onClick={props.onAdd}>Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    function ModalEditAlbum(props) {
+        const album = props.value || {};
+
+        return (
+            <div className="modal fade modal-edit-album" data-id={ album.id } tabIndex="-1" role="dialog" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Редактировать альбом</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="form-group">
+                                <label>Название альбома</label>
+                                <input type="text" className="form-control input-edit-album-name" defaultValue={ album.name } />
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" onClick={ props.onEditSave }>Save changes</button>
                         </div>
                     </div>
                 </div>
@@ -124,6 +156,63 @@
             newAlbumNameInput.val("").blur();
         }
 
+        handleEditStart(id) {
+            id = parseInt(id);
+            if (isNaN(id)) {
+                return;
+            }
+
+            const albums = this.state.albums.slice();
+
+            let albumToEdit = albums.filter(function(elem) {
+                return elem.id === id;
+            });
+            if (!albumToEdit.length) {
+                return;
+            }
+
+            albumToEdit = albumToEdit[0];
+            const editAlbumModal = editAlbumModals.filter("[data-id=" + id + "]");
+            editAlbumModal.modal("show");
+        }
+
+        handleEditSave(id) {
+            id = parseInt(id);
+            if (isNaN(id)) {
+                return;
+            }
+
+            const albums = this.state.albums.slice();
+
+            let albumToSave = albums.filter(function(elem) {
+                return elem.id === id;
+            });
+            if (!albumToSave.length) {
+                return;
+            }
+
+            albumToSave = albumToSave[0];
+            const index = albums.indexOf(albumToSave);
+
+            const editAlbumModal = editAlbumModals.filter("[data-id=" + id + "]");
+            const editAlbumNameInput = editAlbumModal.find(".input-edit-album-name");
+            const newName = editAlbumNameInput.val();
+            albums[index].name = newName;
+
+            this.setState({
+                albums: albums
+            });
+
+            if (localStorage) {
+                localStorage.setItem(albumsStorageName, JSON.stringify(albums));
+            }
+
+            editAlbumModal.modal("hide");
+            editAlbumNameInput.val("").blur();
+
+            console.log("saved album with id: " + id);
+        }
+
         handleRemove(id) {
             id = parseInt(id);
             if (isNaN(id)) {
@@ -165,13 +254,13 @@
 
         render() {
             const albums = this.state.albums.map((elem) => {
-                return <Album key={ elem.id } value={ elem } onRemove={ (id) => this.handleRemove(id) } />
+                return <Album key={ elem.id } value={ elem } onEditStart={ (id) => this.handleEditStart(id) } onEditSave={ (id) => this.handleEditSave(id) } onRemove={ (id) => this.handleRemove(id) } />
             });
 
             return (
                 <div>
                     <div className="pb-5">
-                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#new-album-modal-2">
+                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#modal-new-album">
                             Добавить альбом
                         </button>
 
@@ -196,15 +285,18 @@
         }
 
         componentDidMount() {
-            newAlbumNameInput = $("#new-album-name-2");
-            newAlbumModal = $("#new-album-modal-2");
-
+            newAlbumNameInput = $("#input-new-album-name");
+            newAlbumModal = $("#modal-new-album");
             newAlbumModal.on("shown.bs.modal", function() {
                 newAlbumNameInput.focus();
             });
+
+            editAlbumModals = $(".modal-edit-album");
         }
 
         componentDidUpdate() {
+            editAlbumModals = $(".modal-edit-album");
+
             Holder.run();
             if (localStorage) {
                 localStorage.setItem(albumsStorageName, JSON.stringify(this.state.albums.slice()))
